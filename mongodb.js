61cf6a69161da73b6d39861ba84c7085f81917ee
Obsidian-ig <149707463+Obsidian-ig@users.noreply.github.com>
2025-06-29 23:10:21 -0700

    Update mongodb.js

--- mongodb.js
+++ mongodb.js
@@ -1,6 +1,6 @@
 const { MongoClient, ServerApiVersion } = require("mongodb");
 const uri =
-  "mongodb+srv://obsidianig:Icycrowns283!@obsidianwebcluster.1ug7w.mongodb.net/?retryWrites=true&w=majority&appName=ObsidianWebCluster";
+  "nope";
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
